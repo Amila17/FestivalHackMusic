280bd34bc1475be6532b373ea9d15b858d8e5804
Amila17 <amila.prabandhika@bedegaming.com>
2015-02-20 15:19:14 +0000

    Stream Button

--- app/src/main/java/com/mycompany/festivalhackmusic/MainActivity.java
+++ app/src/main/java/com/mycompany/festivalhackmusic/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -36,4 +37,10 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onStream(View view)
+    {
+        
+    }
+
 }
